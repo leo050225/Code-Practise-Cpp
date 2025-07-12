2b721cf11b67a8cacb3617e96617bbee7d4b6d97
leo050225 <leo050225@gmail.com>
2025-07-12 08:52:28 +0800

    Create LeetCode-977.cpp

---
+++ Code/LeetCode-977.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include<iostream>
+#include<algorithm>
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) 
+    {
+        vector<int> v;
+
+        for(int i : nums)
+        {
+            v.push_back(i*i);
+        }
+        
+        sort(v.begin(),v.end());
+        
+        return v;
+    }
+};
