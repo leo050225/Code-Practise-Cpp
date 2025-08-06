5c09a80f00f5a15dd1e06d8c11ece815d20bed2c
leo050225 <leo050225@gmail.com>
2025-08-06 09:52:43 +0800

    Update LeetCode-454.cpp

--- Code/LeetCode-454.cpp
+++ Code/LeetCode-454.cpp
@@ -34,6 +34,37 @@ public:
     }
 };
 
+// class Solution {
+// public:
+//     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+//         unordered_map<int,int> num_map;
+
+//         int count = 0;
+
+//         for(int a : nums1)
+//         {
+//             for(int b : nums2)
+//             {
+//                 num_map[a+b]++;
+//             }
+//         }
+
+//         for(int c : nums3)
+//         {  
+//             for(int d : nums4)
+//             {
+//                 int target = 0 - (c + d);
+
+//                 if(num_map.find(target) != num_map.end())
+//                 {
+//                     count += num_map[target];
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
 int main()
 {
     return 0;
