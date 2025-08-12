41afaa8bb6382bd525bc7f4f46c9fc512d76fdc7
leo050225 <leo050225@gmail.com>
2025-08-12 10:05:42 +0800

    Create LeetCode-541.cpp

---
+++ Code/LeetCode-541.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+#include<algorithm>
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for(int i = 0; i < s.size(); i+=(2*k))
+        {
+            if(i+k <= s.size())
+            {
+                reverse(s.begin() + i, s.begin() + i + k);
+                continue;
+            }
+            reverse(s.begin() + i, s.end());
+        }
+        return s;
+    } 
+};
+
+int main()
+{
+    return 0;
+}
