46a6d1d5955f3ec3a609fcbcfb15c9418bb30bbf
leo050225 <leo050225@gmail.com>
2025-09-12 10:56:39 +0800

    Create 跌代bool.cpp

---
+++ Code/LeetCode-144/跌代bool.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<stack>
+#include<algorithm>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+                vector<int> result;
+        stack<pair<TreeNode*, bool>> st;
+        if(root != nullptr)
+            st.push(make_pair(root, false));
+
+        while (!st.empty())
+        {   
+            auto node = st.top().first;
+            auto visited = st.top().second;
+            st.pop();
+
+            if(visited)
+            {
+                result.push_back(node->val);
+                continue;
+            }
+
+            if(node->right) st.push(make_pair(node->right, false));
+            if(node->left) st.push(make_pair(node->left, false));
+            st.push(make_pair(node, true));
+                
+        }
+
+    return result;
+    }
+};
+
+int main()
+{
+
+}
