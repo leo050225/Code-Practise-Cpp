2649b540fdd7d698a91cb4077d3fcb7144350f9e
leo050225 <leo050225@gmail.com>
2025-09-13 10:12:37 +0800

    Create LeetCode-102.cpp

---
+++ Code/LeetCode-102.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<queue>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        
+        if(root == nullptr) return result;
+
+        queue<TreeNode*> que;
+
+        int size;
+
+        TreeNode* node;
+
+        que.push(root);
+
+
+        while(!que.empty())
+        {
+            size =  que.size();
+            vector<int> v;
+
+            for(int i = 0; i<size; i++)
+            {
+                node = que.front();
+                que.pop();
+
+                v.push_back(node->val);
+
+                if(node->left)que.push(node->left);
+                if(node->right)que.push(node->right);
+            }
+            result.push_back(v);
+        }
+        return result;
+    }
+};
+
+int main()
+{
+
+}
