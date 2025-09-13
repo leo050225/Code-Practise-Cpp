445b367397a2c49e5bc8cbbeac8c9e9906f8248b
leo050225 <leo050225@gmail.com>
2025-09-13 10:47:15 +0800

    Create LeetCode-637.cpp

---
+++ Code/LeetCode-637.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> result;
+
+        if(root == nullptr)return result;
+
+        queue<TreeNode*> que;
+        que.push(root);
+
+        while(!que.empty())
+        {
+            int size = que.size();
+            double sum = 0;
+
+            for(int i = 0; i<size; i++)
+            {
+                TreeNode* node = que.front();
+                que.pop();
+
+                sum += node->val;
+            
+                if(node->left)que.push(node->left);
+                if(node->right)que.push(node->right);
+            }
+            result.push_back(sum/size);
+        }
+        return result;
+    }
+};
+
+int main()
+{
+
+}
