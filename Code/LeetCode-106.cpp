ab0f3e00c74284e932e68e1588531cb66608c959
leo050225 <leo050225@gmail.com>
2025-09-18 15:33:34 +0800

    Create LeetCode-106.cpp

---
+++ Code/LeetCode-106.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<queue>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* Traversal(vector<int>& inorder, vector<int>& postorder)
+    {   
+        if(postorder.size() == 0)return nullptr;
+        
+        int rootValue = postorder[postorder.size() - 1];
+        TreeNode* root = new TreeNode(rootValue);
+        
+        if(postorder.size() == 1)return root;
+
+        int index = 0;
+        for(index = 0; index<inorder.size(); index++)
+        {
+            if(inorder[index] == rootValue) break;
+        }
+
+        vector<int> leftInorder(inorder.begin(), inorder.begin() + index);
+        vector<int> rightInorder(inorder.begin() + index + 1, inorder.end());
+        
+        postorder.resize(postorder.size() - 1);
+
+        vector<int> leftPostorder(postorder.begin(), postorder.begin() + leftInorder.size());
+        vector<int> rightPostorer(postorder.begin() + leftInorder.size(), postorder.end());
+
+        root->left = Traversal(leftInorder, leftPostorder);
+        root->right = Traversal(rightInorder, rightPostorer);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if(inorder.size() == 0 || postorder.size() == 0)return nullptr;
+
+        return Traversal(inorder, postorder);
+    }
+};
+
+int main()
+{
+
+}
