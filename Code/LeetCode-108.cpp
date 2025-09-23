fee4fc9b53e32a410395408e21a5b8f4470af132
leo050225 <leo050225@gmail.com>
2025-09-23 12:59:13 +0800

    Create LeetCode-108.cpp

---
+++ Code/LeetCode-108.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* Traversal(vector<int>& nums, int left, int right)
+    {
+        if(left > right)return nullptr;
+
+        int mid = (left + right) / 2;
+
+        TreeNode* node = new TreeNode(nums[mid]);
+
+        node->left = Traversal(nums, left, mid - 1);
+        node->right = Traversal(nums, mid + 1, right);
+    
+        return node;
+    }   
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return Traversal(nums, 0, nums.size() - 1);
+    }
+};
+
+int main()
+{
+    
+}
