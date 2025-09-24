d3e7450c21964e1702f7e2a04eedf050cdae823e
leo050225 <leo050225@gmail.com>
2025-09-24 10:40:22 +0800

    Create LeetCode-77.cpp

---
+++ Code/LeetCode-77.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int> nums;
+
+    void backTracking(int n, int k, int index)
+    {
+        if(nums.size() == k)
+        {
+            result.push_back(nums);
+            return;
+        }
+
+        for(int i = index; i <= n; i++)
+        {
+            nums.push_back(i);
+            backTracking(n, k, i + 1);
+            nums.pop_back();
+        }
+    }
+
+
+    vector<vector<int>> combine(int n, int k) {
+        backTracking(n, k, 1);
+        return result;
+    }
+};
+
+int main()
+{
+    
+}
