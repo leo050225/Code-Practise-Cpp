2e8359325b67ecd5c0f1cfc2e4a228410c64ede6
leo050225 <leo050225@gmail.com>
2025-10-06 10:06:25 +0800

    Create LeetCode-122.cpp

---
+++ Code/LeetCode-122.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int result = 0;
+
+        for(int i = 1; i<prices.size(); i++)
+        {
+            result += max(prices[i] - prices[i - 1], 0);
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+}
