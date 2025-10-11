7e9d5def35cfe9787f9314ea36be0db59a8fa789
leo050225 <leo050225@gmail.com>
2025-10-11 09:50:43 +0800

    Update LeetCode-56.cpp

--- Code/LeetCode-56.cpp
+++ Code/LeetCode-56.cpp
@@ -9,29 +9,56 @@ public:
         sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){return a[0] < b[0];});
         
         vector<vector<int>> result;
-
-        int left = intervals[0][0];
-        int right = intervals[0][1];
         
-        for(int i = 1; i<intervals.size(); i++)
+        result.push_back(vector<int>{intervals[0][0], intervals[0][1]});
+
+        for(int i = 0; i<intervals.size(); i++)
         {
-            if(intervals[i][0] <= right)
+            if(intervals[i][0] <= result.back()[1])
             {
-                right = max(right, intervals[i][1]);
+                result.back()[1] = max(intervals[i][1], result.back()[1]); 
             }
             else
             {
-                result.push_back(vector<int>{left, right});
-                left = intervals[i][0];
-                right = intervals[i][1];
+                result.push_back(vector<int>{intervals[i][0], intervals[i][1]});
             }
         }
-        
-        result.push_back(vector<int>{left, right});
 
         return result;
     }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){return a[0] < b[0];});
+        
+//         vector<vector<int>> result;
+
+//         int left = intervals[0][0];
+//         int right = intervals[0][1];
+        
+//         for(int i = 1; i<intervals.size(); i++)
+//         {
+//             if(intervals[i][0] <= right)
+//             {
+//                 right = max(right, intervals[i][1]);
+//             }
+//             else
+//             {
+//                 result.push_back(vector<int>{left, right});
+//                 left = intervals[i][0];
+//                 right = intervals[i][1];
+//             }
+//         }
+        
+//         result.push_back(vector<int>{left, right});
+
+//         return result;
+//     }
+// };
+
+
 
 int main()
 {
