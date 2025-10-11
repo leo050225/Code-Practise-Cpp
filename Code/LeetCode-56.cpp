690d83a91c7dbea4973a008ac4ff3eccb7b8a805
leo050225 <leo050225@gmail.com>
2025-10-11 09:44:14 +0800

    Create LeetCode-56.cpp

---
+++ Code/LeetCode-56.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){return a[0] < b[0];});
+        
+        vector<vector<int>> result;
+
+        int left = intervals[0][0];
+        int right = intervals[0][1];
+        
+        for(int i = 1; i<intervals.size(); i++)
+        {
+            if(intervals[i][0] <= right)
+            {
+                right = max(right, intervals[i][1]);
+            }
+            else
+            {
+                result.push_back(vector<int>{left, right});
+                left = intervals[i][0];
+                right = intervals[i][1];
+            }
+        }
+        
+        result.push_back(vector<int>{left, right});
+
+        return result;
+    }
+};
+
+int main()
+{
+
+}
