3b2dbe85825a620f175e980f8c711cf802c06827
leo050225 <leo050225@gmail.com>
2025-10-14 11:45:49 +0800

    Create LeetCode-62.cpp

---
+++ Code/LeetCode-62.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#include<vector>
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n,1);
+
+        for(int i = 1; i<m; i++)
+        {
+            for(int j = 1; j<n; j++)
+            {
+                dp[j] = dp[j] + dp[j -1];
+            }
+        }
+        return dp[n-1];
+    }
+};
+
+int main()
+{
+    
+}
