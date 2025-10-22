f15a38b1cf8253cc3903a08b55e3b88681904c72
leo050225 <leo050225@gmail.com>
2025-10-22 08:18:16 +0800

    Create LeetCode-322.cpp

---
+++ Code/LeetCode-322.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<uint64_t> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+
+        for(int i = 0; i<coins.size(); i++)
+        {
+            for(int j = coins[i]; j<=amount; j++)
+            {
+                if(dp[j - coins[i]] != INT_MAX)
+                    dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+            }
+        }
+
+
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
+    }
+};
+
+int main()
+{
+
+}
