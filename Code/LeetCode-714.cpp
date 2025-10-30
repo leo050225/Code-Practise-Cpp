deb7b1b7492fd70fe576a67ec191a3cb97e9c49d
leo050225 <leo050225@gmail.com>
2025-10-30 17:16:27 +0800

    Create LeetCode-714.cpp

---
+++ Code/LeetCode-714.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        
+        int len = prices.size();
+
+        vector<vector<int>> dp(len, vector<int>(2,0));
+
+        dp[0][0] = -prices[0];//持股
+        dp[0][1] = 0;//沒有股
+
+        for(int i = 1; i<len; i++)
+        {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee);
+        }
+
+        return dp[len - 1][1];
+    }
+};
+
+int main()
+{
+    
+}
