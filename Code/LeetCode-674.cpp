58a7cc48f1ff8e458d94581f4d74fe323e386d25
leo050225 <leo050225@gmail.com>
2025-10-31 11:38:36 +0800

    Create LeetCode-674.cpp

---
+++ Code/LeetCode-674.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        if(nums.size() == 1)return 1;
+        vector<int> dp(nums.size(), 1);
+
+        int result = 1;
+
+        for(int i = 1; i<nums.size(); i++)
+        {
+            if(nums[i] > nums[i - 1]) dp[i] = dp[i - 1] + 1;
+
+            if(dp[i] > result )result = dp[i];
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    
+}
