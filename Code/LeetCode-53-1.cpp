5f9feb94f33509c4c7b2855c6e05a4bb59468ef1
leo050225 <leo050225@gmail.com>
2025-11-02 10:35:49 +0800

    Create LeetCode-53-1.cpp

---
+++ Code/LeetCode-53-1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+
+        int result = nums[0];
+
+        for(int i = 1; i<nums.size(); i++)
+        {
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+            result = max(result, dp[i]);
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    
+}
