a915e04dcb3461058a6e8379893dfa4ddd26246c
leo050225 <leo050225@gmail.com>
2025-12-08 14:03:58 +0800

    Create KamaCoder-94.cpp

---
+++ Code/KamaCoder-94.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<climits>
+
+int main()
+{
+    int n , m, s, t, val;
+
+    cin>>n>>m;
+
+    vector<vector<int>> grid;
+
+    while(m--)
+    {
+        cin>>s>>t>>val;
+        
+        grid.push_back({s,t,val});
+    }
+
+    vector<int> minDist(n+1, INT_MAX);
+
+    minDist[1] = 0;
+
+    for(int i = 1; i<n; i++)
+    {
+        for(vector<int> &side : grid)
+        {
+            int from = side[0];
+            int to = side[1];
+            int price = side[2];
+
+            if(minDist[from] != INT_MAX && minDist[from] + price < minDist[to])
+            {
+                minDist[to] = minDist[from] + price;
+            }
+        }
+    }
+
+    if(minDist[n] == INT_MAX)
+        cout<<"unconnected"<<endl;
+    else
+        cout<<minDist[n]<<endl;
+}
