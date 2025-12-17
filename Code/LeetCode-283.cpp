db115560f8f38526feb5f2f741c560abf86193ea
leo050225 <leo050225@gmail.com>
2025-12-17 10:38:58 +0800

    Create LeetCode-283.cpp

---
+++ Code/LeetCode-283.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int index = 0;
+
+        for(int i = 0; i<nums.size(); i++)
+        {
+            if(nums[i]){
+                nums[index++] = nums[i];
+            }
+        }
+        while(index<nums.size())
+        {
+            nums[index++] = 0;
+        }
+    }
+};
+
+int main()
+{
+
+}
