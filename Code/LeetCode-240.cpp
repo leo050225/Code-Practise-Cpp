4a93976f9ecae2edc5cfc6438450a1d75f196a03
leo050225 <leo050225@gmail.com>
2026-01-07 09:28:08 +0800

    Create LeetCode-240.cpp

---
+++ Code/LeetCode-240.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int x = 0;
+        int y = matrix[0].size() - 1;
+
+        while(x < matrix.size() && y >= 0)
+        {
+            if(matrix[x][y] == target)
+                return true;
+            else if(matrix[x][y] < target)
+                x++;
+            else
+                y--;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    
+}
