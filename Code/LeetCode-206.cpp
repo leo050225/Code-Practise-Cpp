38c62603c8ad661da978323ff987a1a7730d77aa
leo050225 <leo050225@gmail.com>
2026-01-08 10:16:54 +0800

    Refactor reverseList function with recursion

--- Code/LeetCode-206.cpp
+++ Code/LeetCode-206.cpp
@@ -1,53 +1,90 @@
 #include <iostream>
-using namespace std;
-#include<algorithm>
 #include<vector>
-#include <climits>
-
-
-
+#include<algorithm>
+using namespace std;
 
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
-//雙指針
-class Solution1 {
+    ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+class Solution {
 public:
+ 
     ListNode* reverseList(ListNode* head) {
-        ListNode *cur = head;
-        ListNode *pre = nullptr;
-        while (cur)
-        {
-            ListNode *tmp = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = tmp;
-        }
-        return pre;
-    }
-};
 
-//遞歸
-class Solution2 {
-public:
-    ListNode* reverse(ListNode *cur, ListNode *pre){
-        if(cur==nullptr)return pre;
-        ListNode *tmp = cur->next;
-        cur->next = pre;
-        return reverse(tmp,cur);
-    }
+        if(!head || !head->next)
+            return head;
 
-    ListNode* reverseList(ListNode* head) {
-        return reverse(head,nullptr);
+        ListNode *newhead = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return newhead;
     }
 };
 
 int main()
 {
-    return 0;
+    
 }
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+// #include<algorithm>
+// #include<vector>
+// #include <climits>
+
+
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+
+// //雙指針
+// class Solution1 {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode *cur = head;
+//         ListNode *pre = nullptr;
+//         while (cur)
+//         {
+//             ListNode *tmp = cur->next;
+//             cur->next = pre;
+//             pre = cur;
+//             cur = tmp;
+//         }
+//         return pre;
+//     }
+// };
+
+// //遞歸
+// class Solution2 {
+// public:
+//     ListNode* reverse(ListNode *cur, ListNode *pre){
+//         if(cur==nullptr)return pre;
+//         ListNode *tmp = cur->next;
+//         cur->next = pre;
+//         return reverse(tmp,cur);
+//     }
+
+//     ListNode* reverseList(ListNode* head) {
+//         return reverse(head,nullptr);
+//     }
+// };
+
+// int main()
+// {
+//     return 0;
+// }
