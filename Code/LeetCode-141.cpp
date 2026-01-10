2cc25cd44f33dab7598ce11b8161d25cf420cad0
leo050225 <leo050225@gmail.com>
2026-01-10 09:29:12 +0800

    Create LeetCode-141.cpp

---
+++ Code/LeetCode-141.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ 
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+        if(!head || !head->next)
+            return false;
+        ListNode *fast = head->next;
+        ListNode *slow = head;
+    
+        while(fast != slow)
+        {
+            if(!fast || !fast->next)
+                return false;
+            
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    
+}
