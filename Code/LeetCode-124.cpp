6d5d4e1e6fad30b1bce2df069ceae4343f26732e
leo050225 <leo050225@gmail.com>
2026-02-11 11:25:49 +0800

    Clean up LeetCode-124.cpp by removing comments
    Removed unused includes and commented-out code.

--- Code/LeetCode-124.cpp
+++ Code/LeetCode-124.cpp
@@ -1,28 +1,5 @@
 #include <iostream>
-#include<vector>
-#include<unordered_map>
 using namespace std;
-
-// class Node {
-// public:
-//     int val;
-//     Node* next;
-//     Node* random;
-
-//     Node(int _val) {
-//         val = _val;  
-//         next = NULL;
-//         random = NULL;
-//     }
-// };
-
-// struct ListNode {
-//     int val;
-//     ListNode *next;
-//     ListNode() : val(0), next(nullptr) {}
-//     ListNode(int x) : val(x), next(nullptr) {}
-//     ListNode(int x, ListNode *next) : val(x), next(next) {}
-// };
 
 struct TreeNode {
     int val;
