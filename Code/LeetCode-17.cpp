fd9ab45139a416c3194e80565c0a288d9a52b70e
leo050225 <leo050225@gmail.com>
2026-02-20 13:40:06 +0800

    Refactor letter combinations solution for clarity

--- Code/LeetCode-17.cpp
+++ Code/LeetCode-17.cpp
@@ -1,39 +1,39 @@
 #include <iostream>
-using namespace std;
 #include<vector>
+using namespace std;
 
 class Solution {
 public:
-    const string letterMap[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    const vector<string> mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
-    vector<string> result;//結果
-
-    string str;//紀錄
-
-
-    void backTracking(string digits, int index)
-    {
-        if(digits.length() == index)
+    vector<string> letterCombinations(string digits) {
+        if(digits == "")
         {
-            result.push_back(str);
+            return {};
+        }
+
+        vector<string> res;
+        string path;
+        backtrack(digits, 0, res, path);
+        return res;
+    }
+
+    void backtrack(const string& digits, int index, vector<string>& res, string& path)
+    {
+        if(index >= digits.size())
+        {
+            res.push_back(path);
             return;
         }
 
-        int digit = digits[index] - '0';
-        string letter = letterMap[digit];
+        const string& letters = mapping[digits[index] - '0'];
 
-        for(int i = 0; i < letter.size(); i++)
+        for(char c : letters)
         {
-            str.push_back(letter[i]);
-            backTracking(digits, index + 1);
-            str.pop_back();
+            path.push_back(c);
+            backtrack(digits, index+1, res, path);
+            path.pop_back();
         }
-    }
-
-    vector<string> letterCombinations(string digits) {
-        if(digits.empty())return result;
-        backTracking(digits, 0);
-        return result;
     }
 };
 
@@ -41,3 +41,4 @@ int main()
 {
     
 }
+
