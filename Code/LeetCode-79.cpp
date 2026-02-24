cdb6f517156db0e18667e50e34cc13e6e7ba902a
leo050225 <leo050225@gmail.com>
2026-02-24 10:47:37 +0800

    Implement word search algorithm in LeetCode-79

---
+++ Code/LeetCode-79.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i = 0; i<board.size(); i++)
+        {
+            for(int j = 0; j<board[0].size(); j++)
+            {
+                if(dfs(board, word, i, j, 0))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool dfs(vector<vector<char>>& board, const string& word, int r, int c, int index)
+    {
+        if(index == word.size())
+        {
+            return true;
+        }
+
+        if(r < 0 || r >= board.size() || c < 0 || c >= board[0].size() || board[r][c] != word[index])
+        {
+            return false;
+        }
+
+        char temp = board[r][c];
+        board[r][c] = '\0';
+
+        bool found = dfs(board, word, r + 1, c, index+1)||
+                    dfs(board, word, r - 1, c, index+1)||
+                    dfs(board, word, r, c + 1, index+1)||
+                    dfs(board, word, r, c - 1, index+1);
+
+        board[r][c] = temp;
+
+        return found;
+    }
+};
+
+int main()
+{
+
+}
