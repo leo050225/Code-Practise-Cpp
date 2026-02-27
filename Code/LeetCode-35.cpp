2021bbaecd59eb783c22c25ba8be015977dbb102
leo050225 <leo050225@gmail.com>
2026-02-27 11:35:41 +0800

    Create LeetCode-35.cpp

---
+++ Code/LeetCode-35.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid;
+        while(left <= right)
+        {
+            mid =  left + (right - left) / 2;
+
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
+
+int main()
+{
+    
+}
+
